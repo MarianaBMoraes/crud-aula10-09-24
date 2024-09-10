424445449ca9b2652454acfe1967e524d6089f2c
MarianaBMoraes <marianabmoraes.ef@gmail.com>
2024-09-10 20:30:30 -0300

    initial commit

---
+++ src/db.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const mongoDB = "mongodb+srv://mariana_bmoraes:mDWPwYZKokY0Q9Lh@cluster0.kqjgr.mongodb.net/"
+
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+
+main()
+  .then(() => console.log("Conectado com sucesso!"))
+  .catch((err) => console.log(err));
+
+module.exports = mongoose;
---
+++ src/routes/caneta_route.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+
+const caneta_controller = require("../controllers/caneta_controller");
+
+router.post("/", caneta_controller.store);
+router.get("/", caneta_controller.index);
+router.get("/:id", caneta_controller.show);
+router.put("/:id", caneta_controller.update);
+router.delete("/:id", caneta_controller.destroy);
+
+module.exports = router
+
+
